0353074918941b4f575ece1ec57750488bb8a6de
Michele Locati <michele@locati.it>
2023-09-22 15:24:00 +0200

    Version 1.3.1

--- controller.php
+++ controller.php
@@ -16,7 +16,7 @@ class Controller extends Package
 
     protected $appVersionRequired = '8.3.0';
 
-    protected $pkgVersion = '1.3.0';
+    protected $pkgVersion = '1.3.1';
 
     public function getPackageName()
     {
